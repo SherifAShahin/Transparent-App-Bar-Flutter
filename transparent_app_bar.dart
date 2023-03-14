7d768e762779d8d483915085eee0fb94314901a1
Sherif A Shahin <125300187+SherifAShahin@users.noreply.github.com>
2023-03-14 18:52:25 +0200

    Create transparent_app_bar.dart

---
+++ transparent_app_bar.dart
@@ -0,0 +1,14 @@
+AppBar(
+        leading: IconButton(
+          onPressed: ()
+          {
+          
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      )
